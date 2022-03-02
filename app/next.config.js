8c386d72bf1c24b1d42558d7ad13f20f659de3fd
iwamoto65 <kosuke00940@icloud.com>
2022-03-02 20:08:22 +0900

    環境変数を定義した

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    ...require(`./config/${process.env.APP_ENV || 'local'}.json`),
+  },
 }
 
 module.exports = nextConfig
